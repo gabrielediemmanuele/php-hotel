51e472930959382c5cb7995328a73becf90cc790
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-26 15:36:05 +0200

    feat: add bootstrap link

--- main.php
+++ main.php
@@ -1,3 +1,4 @@
+<!-- PHP -->
 <!-- array -->
 <?php
 
@@ -43,3 +44,23 @@
 
     var_dump($hotels);
 ?>
+
+<!-- HTML -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP HOTELS</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Gabriele Di Emmanuele" />
+    <meta name="description" content="php-hotel" />
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
